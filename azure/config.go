abe8df882347132b35dd5c701ad05ffff40ba034
Namrata Chougule <namrata.chougule@think360.ai>
2021-12-21 17:05:31 +0530

    changed azure connection

--- azure/config.go
+++ azure/config.go
@@ -3,8 +3,8 @@ package azure
 import "fmt"
 
 func GetAccountInfo() (string, string, string, string) {
-	azrKey := "uGFxW6GfRg5Zg6hlHxDPARTpcy4k3AJv0ibn+qmGh6ZUICQYYnRo2et+kiUqAmo48/cYlYSIOjwjC7Mxk5VwEw=="
-	azrBlobAccountName := "csg10032001a70f2ae0"
+	azrKey := "ViTt18iPUaTSwdXr2NFifhxzxiDuYh/3xHyyL/O5GcZy/FwXfuGT6IxVhyy/5m6FPB5pNS5bqRehKzp7kW4qvA=="
+	azrBlobAccountName := "algo360test"
 	azrPrimaryBlobServiceEndpoint := fmt.Sprintf("https://%s.blob.core.windows.net/", azrBlobAccountName)
 	azrBlobContainer := "flowxpert-blobs"
 
